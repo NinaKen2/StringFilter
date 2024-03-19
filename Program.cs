7458a3801e4b985a675141425031602ab3c80bdb
NinaKen2 <145197551+NinaKen2@users.noreply.github.com>
2024-03-19 10:19:45 +0300

    Выделение фильтрации в отдельный метод

--- Program.cs
+++ Program.cs
@@ -5,12 +5,11 @@ class Homework {
     // Main Method 
     public static void Main() 
     { 
+        // исходные данные
+        string[] stringarr = new string[] {"array", "ab", "ababa", "aaa", "bb20", "BBB", "BBBB"};  
         // минимальная длина строк для фильтрации
         int max_length = 3;
 
-        // исходные данные
-        string[] stringarr = new string[] {"a", "aa", "aaa", "aaaa", "bb", "BBB", "BBBB"};  
-      
         printArray("input", stringarr); 
 
         string[] short_strings = new string[stringarr.Length];
@@ -23,17 +22,37 @@ class Homework {
                 short_strings[new_count++] = stringarr[i];
         }     
 
-        string[] result = new string[new_count];
-
-        Array.Copy(short_strings, 0, result, 0, new_count);
-
+        string[] result = filterShortStrings(stringarr, max_length);
         printArray("result", result); 
     } 
 
+    // вывод массива строк в консоль
     public static void printArray(string tag, string[] data)
     {
         Console.WriteLine(tag); 
-        foreach (string value in data)
-            Console.WriteLine(value); 
+
+        if (data.Length == 0)
+            Console.WriteLine("[empty array]");
+        else
+            foreach (string value in data)
+                Console.WriteLine(value); 
+    }
+
+    // фильтрация коротких строк
+    public static string[] filterShortStrings(string[] data, int max_len)
+    {
+        string[] short_strings = new string[data.Length];
+
+        int new_count = 0;
+
+        for (int i = 0; i < data.Length; ++i)
+        {
+            if (data[i].Length <= max_len)
+                short_strings[new_count++] = data[i];
+        }     
+
+        string[] result = new string[new_count];
+        Array.Copy(short_strings, 0, result, 0, new_count);
+        return result;
     }
 } 
